6858fa4c4573b4ae44cbcf0a0af79c1de68e4de9
Orlando Villegas <orvigas@gmail.com>
2025-01-05 13:26:39 -0600

    Fixed imports

--- src/main/java/com/example/product/controllers/ProductController.java
+++ src/main/java/com/example/product/controllers/ProductController.java
@@ -4,7 +4,6 @@ import com.example.product.models.Product;
 import com.example.product.services.ProductService;
 import jakarta.validation.Valid;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
