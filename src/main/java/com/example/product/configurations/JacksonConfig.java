79105ff303b8e39b60fdcc107dcf205ccb13d6dd
Orlando Villegas <orvigas@gmail.com>
2025-01-22 19:23:01 -0600

    Added JacksonConfig

---
+++ src/main/java/com/example/product/configurations/JacksonConfig.java
@@ -0,0 +1,9 @@
+package com.example.product.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class JacksonConfig {
+}
